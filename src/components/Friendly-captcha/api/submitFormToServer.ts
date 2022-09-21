dc9bbb19837122f5ba4b01a4c0dc9de4d72be735
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2022-09-21 16:17:25 +0200

    Move secrets to .env, update README.md, add tailwind ass peerDep.

--- src/components/Friendly-captcha/api/submitFormToServer.ts
+++ src/components/Friendly-captcha/api/submitFormToServer.ts
@@ -18,8 +18,6 @@ type CaptchaResponseProps = {
   errors: any;
 };
 
-const API_KEY = 'A1T925NVFO6O60L1OT3S9CPBSRG3HAMUMTDBPIGSFRH4ORCN9CSIS9RBRA';
-
 function submitFormToServer(data: FormDataProps<ExampleForm>) {
   //TODO: check res status code -> let form pass if code != 200
   checkFormValidation(data.captcha).then((res: CaptchaResponseProps) => {
@@ -39,7 +37,7 @@ async function checkFormValidation(
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       solution: optionValues.solution,
-      secret: API_KEY,
+      secret: process.env.REACT_APP_AACN_FC_API_KEY,
       sitekey: optionValues.sitekey,
     }),
   };
