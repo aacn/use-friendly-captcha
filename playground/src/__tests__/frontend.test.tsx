e56d05b32c61dcfd101b3ffda560b6e709e39b85
Maurice Arikoglu <MauriceArikoglu@users.noreply.github.com>
2022-11-24 17:35:15 +0100

    Update tests

---
+++ playground/src/__tests__/frontend.test.tsx
@@ -0,0 +1,14 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useCaptchaHook } from "@aacn/use-friendly-captcha";
+
+const DEMO_SITEKEY = "FC123456789ABC";
+
+describe('useCaptchaHook', () => {
+    const captchaManager = renderHook(() => useCaptchaHook({siteKey: DEMO_SITEKEY, showAttribution: false }));
+
+    test('should return the initial status of the captcha', () => {
+        expect(captchaManager.result.current.captchaStatus).toEqual({ solution: null, error: null });
+    });
+});
+
+export {};
